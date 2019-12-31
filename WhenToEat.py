c88691c82dc043f0c5492b54fdb63b84c5b74348
JacksonJ01 <jacksonj@hartwick.edu>
2019-12-30 19:19:50 -0500

    Made everything a lot better

--- WhenToEat.py
+++ WhenToEat.py
@@ -12,14 +12,48 @@ def rules():
           "\nMathematically yes, but not here")
 
 
-def eat():
+def number_of_meals():
+    input("Press Enter")
+    meals = randint(1, 4)
+    print("Looks like you'll have " + str(meals) + " for the day")
+    happy = input("Are you happy with this number"
+                  "\n>>>").title()
+    if happy == "Yes" or happy == "Y":
+        print("Good")
+    elif happy == "No" or happy == "N":
+        while happy != "Yes" or happy != "Y":
+            meals = randint(1, 4)
+            happy = input(f"Your new number is {meals}"
+                          f"\nAre happy with this number?"
+                          f"\n>>>").title()
+            if happy == "Yes" or happy == "Y":
+                print(f"Good,", meals, "it is")
+                break
+
+    if meals == 1:
+        print("You will need to eat a bigger meal for today"
+              "\nTwo plates of food doesn't sound like much but you shouldn't over eat")
+        meals1.append(1)
+    elif meals == 2:
+        print("For each meal you should eat one plate of food")
+        meals1.append(2)
+    elif meals == 3:
+        print("2/3 a plate to 3/4 of a plate for your meals")
+        meals1.append(3)
+    elif meals == 4:
+        print("You will need to eat smaller meals today"
+              "\n1/2 a plate maximum for each meal")
+        meals1.append(4)
+
+
+def when_to_eat():
     print(meals1[0])
     Meals["Breakfast"] = input("Do you want to eat Breakfast?"
                                "\n>>>").title()
     Meals["Lunch"] = input("What about Lunch?"
                            "\n>>>").title()
     Meals["Dinner"] = input("And is Dinner on the table"
-                            "\n>>>")
+                            "\n>>>").title()
     if Meals["Breakfast"] == "Yes" or Meals["Breakfast"] == "Y"\
             and Meals["Lunch"] == "Yes" or Meals["Lunch"] == "Y"\
             and Meals["Dinner"] == "Yes" or Meals["Dinner"] == "Y":
@@ -39,6 +73,27 @@ def eat():
         print("")
     elif Meals["Dinner"] == "Yes" or Meals["Dinner"] == "Y":
         print("")
+
+
+def interface():
+    print("\nINTERFACE")
+    do = input("What do you want to do?"
+               "\n-Rules"
+               "\n-Number Of Meals"
+               "\n-When To Eat"
+               "\n-Exit").title()
+
+    if do == "Rules" or do == "R":
+        print("Okay, here are the rules")
+        rules()
+        interface()
+    elif do == "Number Of Meals" or do == "Number" or do == "Of" or do == "Meals" or do == "N" or do == "O" or do == "M":
+        number_of_meals()
+    elif do == "When To Eat" or do == "When" or do == "To" or do == "Eat" or do == "W" or do == "T" or do == "E":
+        when_to_eat()
+    elif do == "Exit":
+        print("See you later")
+        exit()
 
 
 input("*Click Here Then Press Enter*")
@@ -105,52 +160,3 @@ else:
                     elif go == "No" or go == "N":
                         print("Okay, have fun, party pooper")
                         exit()
-
-print("Okay, so in this program, the more meals you have in a day, the smaller the portions")
-input("Press Enter")
-meals = randint(1, 4)
-print("Looks like you'll have " + str(meals) + " for the day")
-happy = input("Are you happy with this number"
-              "\n>>>").title()
-if happy == "Yes" or happy == "Y":
-    print("Good")
-elif happy == "No" or happy == "N":
-    while happy != "Yes" or happy != "Y":
-        meals = randint(1, 4)
-        happy = input(f"Your new number is {meals}"
-                      f"\nAre happy with this number?"
-                      f"\n>>>").title()
-        if happy == "Yes" or happy == "Y":
-            print(f"Good,", meals, "it is")
-            break
-
-if meals == 1:
-    print("You will need to eat a bigger meal for today"
-          "\nTwo plates of food doesn't sound like much but you shouldn't over eat")
-    meals1.append(1)
-elif meals == 2:
-    print("For each meal you should eat one plate of food")
-    meals1.append(2)
-elif meals == 3:
-    print("2/3 a plate to 3/4 of a plate for your meals")
-    meals1.append(3)
-elif meals == 4:
-    print("You will need to eat smaller meals today"
-          "\n1/2 a plate maximum for each meal")
-    meals1.append(4)
-
-when = input("Do you also need to know when to eat?"
-             "\n>>>").title()
-if when == "Yes" or when == "Y":
-    eat()
-elif when == "No" or when == "N":
-    print("Alright then, I will now end the program")
-    exit()
-else:
-    while when != "Yes" or when != "Y" or when != "No" or when != "N":
-        when = input("Do you want to know when to eat?"
-                     "\n>>>").title()
-        if when == "Yes" or when == "Y":
-            eat()
-        elif when == "No" or when == "N":
-            print("Okay then")
